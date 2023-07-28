c03b6ee8b88dcc4cbf68aeb9d0552a292f033adf
Zurban0 <joseignaciozurbano@gmail.com>
2023-07-28 10:27:44 +0200

    Ejemplo relojes eventos

---
+++ EjemploDelegadosEventosLINQ/EjemploEventos/Program.cs
@@ -0,0 +1,21 @@
+﻿using EntidadesEventos;
+
+namespace EjemploEventos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Reloj reloj = new();
+            RelojDigital relojDigital = new();
+            relojDigital.Suscribir(reloj);
+
+            var log = new Registro();
+            log.Suscribir(reloj);
+
+
+            reloj.IniciarReloj();
+
+        }
+    }
+}
---
+++ EjemploDelegadosEventosLINQ/EntidadesDelegados/InformacionTiempoEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace EntidadesEventos
+{
+    public class InformacionTiempoEventArgs
+    {
+        public InformacionTiempoEventArgs(int hora, int minuto, int segundo)
+        {
+            Hora=hora;
+            Minuto=minuto;
+            Segundo=segundo;
+        }
+
+        public int Hora { get; set; }
+        public int Minuto { get; set; }
+        public int Segundo { get; set; }
+
+    }
+}
